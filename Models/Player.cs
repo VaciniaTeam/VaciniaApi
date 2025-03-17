c7e2a81ee33d3cba076b3f3665be77431719377d
ClonerNotFound <clonernotfound@gmail.com>
2025-03-18 00:25:04 +0300

    Добавьте файлы проекта.

---
+++ Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace VaciniaApi.Models
+{
+    public class Player
+    {
+        public string? LastName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int TotalPlayedSeconds { get; set; }
+    }
+}
